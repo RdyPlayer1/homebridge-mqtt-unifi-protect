a8a16ff0c567fe62ec12439aafa1a4a8545d2ddc
RdyPlayer1 <nboyer+github@gmail.com>
2026-02-23 15:59:51 -0500

    Update accessory.js

--- accessory.js
+++ accessory.js
@@ -1,96 +1,65 @@
 let Service, Characteristic;
 
-class AlarmAccessory {
+class Accessory {
   constructor(platform, name, mac, config) {
     this.platform = platform;
     this.log = platform.log;
-    this.api = platform.api;
-
-    Service = this.api.hap.Service;
-    Characteristic = this.api.hap.Characteristic;
-
     this.name = name;
     this.mac = mac;
     this.config = config;
 
+    Service = platform.api.hap.Service;
+    Characteristic = platform.api.hap.Characteristic;
+
+    this.service = new Service(config.type === 'motion' ? Service.MotionSensor : Service.ContactSensor, name);
+
+    this.isArmedHome = config.armHome || false;
+    this.isArmedAway = config.armAway || false;
+    this.monitorOff = config.monitorOff || false;
     this.entryDelay = config.entryDelay || 0;
-    this.armHome = config.armHome ?? true;
-    this.armAway = config.armAway ?? true;
-    this.monitorOff = config.monitorOff ?? false;
-
-    this.createServices();
   }
 
-  createServices() {
-    if (this.config.type === "motion") {
-      this.sensorService = new Service.MotionSensor(this.name);
-    } else {
-      this.sensorService = new Service.ContactSensor(this.name);
-    }
+  handleMessage(value) {
+    const systemState = this.getSystemState();
+    let shouldAlert = false;
 
-    if (this.platform.enableSiren && !this.platform.sirenService) {
-      this.platform.sirenService = new Service.Switch("Alarm Siren");
+    if (systemState === 'home') shouldAlert = this.isArmedHome;
+    else if (systemState === 'away') shouldAlert = this.isArmedAway;
+    else if (systemState === 'off') shouldAlert = this.monitorOff;
 
-      this.platform.sirenService
-        .getCharacteristic(Characteristic.On)
-        .onSet(this.handleSiren.bind(this));
-    }
-  }
+    if (shouldAlert) {
+      const HAP = Characteristic;
 
-  handleMessage(payload) {
-    const [type, valueRaw] = payload.split("=");
-    const active = valueRaw?.trim().toLowerCase() === "true";
-
-    if (!active) return;
-    if (!this.isArmed()) return;
-
-    if (this.entryDelay > 0) {
-      setTimeout(() => this.trigger(), this.entryDelay * 1000);
-    } else {
-      this.trigger();
-    }
-  }
-
-  isArmed() {
-    const state = this.platform.currentState;
-
-    switch (state) {
-      case Characteristic.SecuritySystemCurrentState.STAY_ARM:
-        return this.armHome;
-      case Characteristic.SecuritySystemCurrentState.AWAY_ARM:
-        return this.armAway;
-      case Characteristic.SecuritySystemCurrentState.DISARMED:
-        return this.monitorOff;
-      default:
-        return false;
-    }
-  }
-
-  trigger() {
-    this.platform.currentState =
-      Characteristic.SecuritySystemCurrentState.ALARM_TRIGGERED;
-
-    this.platform.publishAlarmState("triggered");
-    this.platform.saveState();
-
-    if (this.platform.sirenService) {
-      this.platform.sirenService
-        .getCharacteristic(Characteristic.On)
-        .updateValue(true);
-    }
-
-    setTimeout(() => {
-      if (this.platform.sirenService) {
-        this.platform.sirenService
-          .getCharacteristic(Characteristic.On)
-          .updateValue(false);
+      if (this.config.type === 'motion') {
+        this.service.setCharacteristic(HAP.MotionDetected, value === 'true');
+      } else {
+        this.service.setCharacteristic(HAP.ContactSensorState, value === 'true' ? HAP.ContactSensorState.CONTACT_NOT_DETECTED : HAP.ContactSensorState.CONTACT_DETECTED);
       }
-    }, this.platform.alarmDuration * 1000);
+
+      this.log(`${this.name} triggered!`);
+
+      // Trigger alarm after entry delay
+      setTimeout(() => {
+        this.platform.triggerAlarm(HAP.SecuritySystemCurrentState.AWAY_ARM);
+      }, this.entryDelay * 1000);
+    }
   }
 
-  handleSiren(value) {
-    this.platform.publishAlarmState(value ? "siren_on" : "siren_off");
+  getSystemState() {
+    const alarm = this.platform.accessories.find(a => a.getService(Service.SecuritySystem));
+    if (!alarm) return 'off';
+
+    const service = alarm.getService(Service.SecuritySystem);
+    const HAP = Characteristic;
+    const state = service.getCharacteristic(HAP.SecuritySystemCurrentState).value;
+
+    switch(state) {
+      case HAP.SecuritySystemCurrentState.DISARMED: return 'off';
+      case HAP.SecuritySystemCurrentState.AWAY_ARM: return 'away';
+      case HAP.SecuritySystemCurrentState.STAY_ARM: return 'home';
+      default: return 'off';
+    }
   }
 }
 
-module.exports = AlarmAccessory;
+module.exports = Accessory;
